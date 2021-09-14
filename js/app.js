a1d07e2852725855d0456b3036bc47ce79317b44
C.Renauld Rodney <c.renauld.rodney@gmail.com>
2021-09-14 14:06:45 -0500

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 console.log("connected"); // CONNECTED!
+
+// DOM ELEMENTS
+
+// input display window
+const SPAN = document.getElementById("display-window");
+
+// input buttons
+const INPUTBUTTON = document.getElementsByTagName("input");
+
+// TEST ELEMENTS
+// console.log(`span:`, SPAN);
+// console.log(`input buttons:`, INPUTBUTTON);
+
+// attach eventlisteners to each input button...
+for (let i = 0; i < INPUTBUTTON.length; i++) {
+  INPUTBUTTON[i].addEventListener("click", () => {
+    // show value of button clicked @ display window
+    SPAN.innerText = INPUTBUTTON[i].value;
+  });
+}
